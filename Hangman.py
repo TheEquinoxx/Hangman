3ba9ae96b67f9db6f5297a77db94d3f2cd4f16cf
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-12 13:10:56 +1000

    Doc: Complete Hangman Game

--- Hangman.py
+++ Hangman.py
@@ -1,15 +1,46 @@
 import random as rdm
 import string
 
-#Player 2 gives Player 1 a word to guess.
-Word = list(input('Input a valid word to begin the game: ').upper())
-#Adds Alphabet list to crossreference Given word.
-Alphabet = list(string.ascii_uppercase)
-#Check that the word is valid.
-Valid = all(e in Alphabet for e in Word)
-#if word given was invalid loops until a valid word is given
-while Valid != True:
-    Word = list(input('Input a valid word to begin the game: ').upper())
-    Alphabet = list(string.ascii_uppercase)
-    Valid = all(e in Alphabet for e in Word)
+def valid_word():
+    valid = False
+    while valid == False:
+        word = (input("Input a valid word to begin the game: ").upper())
+        word_letters = set(word)
+        alphabet = set(string.ascii_uppercase)
+        valid = all(e in alphabet for e in word_letters)
+    return (word)
 
+def hangman():
+    word = valid_word()
+    word_letters = set(word)
+    alphabet_letters = set(string.ascii_uppercase)
+    used_letters = set()
+    lives = 10
+
+    while len(word_letters) > 0 and lives > 0:
+        print(f"Lives Left: {lives}")
+        print("you have used these letters: ", " ".join(used_letters))
+        hidden_word = [letter if letter in used_letters else "_" for letter in word]
+        print("Current Word: ", " ".join(hidden_word))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet_letters - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print("Letter is not in the word!")
+        elif user_letter in used_letters:
+            print(f"You have already used the letter, {user_letter}, Try again!")
+        else:
+            print("Invalid Character, Try again!")
+
+    hidden_word = [letter if letter in used_letters else "_" for letter in word]
+    print("Current Word: ", " ".join(hidden_word))
+    if lives == 0:
+        print("You Died!")
+    else:
+        print("Congratulation, You Won!")
+
+hangman()
